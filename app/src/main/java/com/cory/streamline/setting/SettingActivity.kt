03e2fc740665133ba64e578f4d8c26809501db19
Cory <625283654@qq.com>
2020-07-22 04:59:51 +0800

    Add setting icons,adjust timestamp layout.

--- app/src/main/java/com/cory/streamline/setting/SettingActivity.kt
+++ app/src/main/java/com/cory/streamline/setting/SettingActivity.kt
@@ -19,12 +19,20 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setting)
         supportActionBar!!.title = "设置"
         val listView = findViewById<ListView>(R.id.listView)
-        val dir=
+        val dir =
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                .path+"/Streamline/"
+                .path + "/Streamline/"
         val list = listOf(
-            SettingListAdapter.Option("文件存储位置", dir, R.drawable.ic_back),
-            SettingListAdapter.Option("自定义Wallpaper详情布局", null, R.drawable.ic_back)
+            SettingListAdapter.Option(
+                "文件存储位置",
+                dir,
+                R.drawable.ic_folder
+            ),
+            SettingListAdapter.Option(
+                "自定义Wallpaper详情布局",
+                null,
+                R.drawable.ic_pencil_ruler
+            )
         )
         listView.adapter = SettingListAdapter(this, list)
     }
